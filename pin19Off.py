cf580fda1928e2b65c7ce1f5db04ce3759a25d3e
James Eggleston <jamesmeggleston@yahoo.co.uk>
2016-08-03 19:24:44 +0200

    added control for pin 19

---
+++ pin19Off.py
@@ -0,0 +1,5 @@
+import setPin as setPin
+
+print "pin19Off: main"
+setPin.setGpioOutPinState(19, False)
+    
---
+++ pin19On.py
@@ -0,0 +1,8 @@
+import setPin as setPin
+
+print "pin19On: main"
+setPin.setGpioOutPinState(19, True)
+    
+
+
+
