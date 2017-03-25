bbac1da9b5a41e6e3c49bb1de7953042fbf41ce0
Eggie <jamesmeggleston@yahoo.co.uk>
2017-03-25 22:37:51 +0100

    use system not exec

--- webserver.py
+++ webserver.py
@@ -67,14 +67,14 @@ class livingRoomLightsOn:
     def GET(self):
         print ("Turning Living Room Lights On...")
         print ("Sending groupswrite ip:localhost 0/0/3 0x01")
-        os.execl("knxtool groupswrite ip:localhost 0/0/3 0x01")
+        os.system("knxtool groupswrite ip:localhost 0/0/3 0x01")
         return render.welcome("Turning Living Room Lights On...");
 
 class livingRoomLightsOff:
     def GET(self):
         print ("Turning Living Room Lights Off...")
         print ("Sending groupswrite ip:localhost 0/0/3 0x00")
-        os.execl("knxtool groupswrite ip:localhost 0/0/3 0x00")
+        os.system("knxtool groupswrite ip:localhost 0/0/3 0x00")
         return render.welcome("Turning Living Room Lights Off...");
 
 
